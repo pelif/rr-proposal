3759146097352bb30bc3eee8f40fe9c2ca5f1c64
Felipe Daniel <felipe.dandev@gmail.com>
2020-06-10 12:35:42 -0300

    layout internal system ok

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import loginIndex from './controller/login/index'
+import home from './controller/home/index'
 
 module.exports  = (app) => {
     app.use('/login', loginIndex)
+    app.use('/', home)
 }
