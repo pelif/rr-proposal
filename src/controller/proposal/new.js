d5bf531819178f9d232c4315a5194a6fe56c6fde
Felipe Daniel <felipe.dandev@gmail.com>
2020-09-14 10:25:03 -0300

    corrigindo erro proposal_number null

--- src/controller/proposal/new.js
+++ src/controller/proposal/new.js
@@ -4,7 +4,12 @@ export default (req, res) => {
     
     Proposal
         .findOne({}, [], { sort: {'created_at': -1} }, function(err, prop) {
-            let lastNumber = parseInt(prop.proposal_number)             
+            
+            let lastNumber = 1000;; 
+            if(prop !== null) {
+                lastNumber = parseInt(prop.proposal_number)        
+            }            
+            
             lastNumber++            
             let viewParams = { proposalNumber: lastNumber }
             if(req.query.msg) {
